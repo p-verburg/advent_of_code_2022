69351261555e1ce9689dc0df98d1ef25b34f8ff4
Paul Verburg <verburg@qps.nl>
2022-12-04 22:15:06 +0100

    Day #4 - Solve Part Two

--- puzzles/day_4/__init__.py
+++ puzzles/day_4/__init__.py
@@ -5,3 +5,7 @@ schedule = read_cleanup_schedule('cleanup_schedule.txt')
 count = sum([1 for pair in schedule.pairs if pair.are_fully_overlapping()])
 
 print(count)
+
+count = sum([1 for pair in schedule.pairs if pair.are_partially_overlapping()])
+
+print(count)
